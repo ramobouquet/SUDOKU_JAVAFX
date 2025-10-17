622f3251af5bc7fe597f92982cd14d03add933fd
Julian Meneses <julianes2003@gmail.com>
2025-10-17 10:59:09 -0500

    Implementacion de la Interfaz para alertas y advertencias

---
+++ src/main/java/Models/IAlertBox.java
@@ -0,0 +1,6 @@
+package Models;
+
+public interface IAlertBox {
+    void showAlertBox(String title, String message, String header);
+    void showWarningAlertBox(String title, String message, String header);
+}
