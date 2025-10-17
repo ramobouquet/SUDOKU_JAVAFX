cb5e855cdba68da34761430c0b3b40977492d565
Julian Meneses <julianes2003@gmail.com>
2025-10-17 11:00:06 -0500

    Implementacion de la clase para utilizar las alertas durante todo el codigo

---
+++ src/main/java/Models/AlertBox.java
@@ -0,0 +1,23 @@
+package Models;
+
+import javafx.scene.control.Alert;
+
+public class AlertBox implements IAlertBox {
+    @Override
+    public void showAlertBox(String title, String message, String header){
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle(title);
+        alert.setHeaderText(header);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+
+    @Override
+    public void showWarningAlertBox(String title, String message, String header){
+        Alert alert = new Alert(Alert.AlertType.WARNING);
+        alert.setTitle(title);
+        alert.setHeaderText(header);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+}
